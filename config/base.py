241f925760ee1aa19370d680d75f2c933408451c
CharlesJonah <mutua.charles48@gmail.com>
2021-09-04 16:40:19 +0300

    add new environment variables

--- config/base.py
+++ config/base.py
@@ -5,3 +5,5 @@ class BaseConfig:
     """This class configures the parameters to be used across different enviroments"""
 
     SECRET_KEY = os.environ["SECRET_KEY"]
+    PROJECT_ID = os.environ["PROJECT_ID"]
+    GOOGLE_APPLICATION_CREDENTIALS = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
