f4708bc8bbcc47a12fafb769bf29465f5b28dffe
CharlesJonah <mutua.charles48@gmail.com>
2021-09-04 16:39:30 +0300

    add new routes with their associated views

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     load_dotenv()
 
     from flask_restful import Api
-    from views import TotalSessions
+    from views import TotalSessions, SessionsPerUser, TimeToOrderConfirmation
 
     from constants import config
     from application import application
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     api = Api(application, catch_all_404s=True)
 
     api.add_resource(TotalSessions, "/api/v1/total-sessions")
-    # api.add_resource(LogComplexSearch, "/search-complex/v1/<string:expression>")
+    api.add_resource(SessionsPerUser, "/api/v1/sessions-per-user")
+    api.add_resource(TimeToOrderConfirmation, "/api/v1/time-to-order-confirmation")
 
-    application.run(host="0.0.0.0", debug=False)
+    application.run(host="0.0.0.0", debug=True)
