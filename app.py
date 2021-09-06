effbda86f6e5b810101d8c496c69dad716f51ddd
CharlesJonah <mutua.charles48@gmail.com>
2021-09-06 17:18:28 +0300

    add new endpoits to suppport querying GetCoordinatesChange, OrdersPlacedWithCoordinatesChange, GetOrderDetailsPerFullVisitorId

--- app.py
+++ app.py
@@ -8,7 +8,14 @@ if __name__ == "__main__":
     load_dotenv()
 
     from flask_restful import Api
-    from views import TotalSessions, SessionsPerUser, TimeToOrderConfirmation
+    from views import (
+        TotalSessions,
+        SessionsPerUser,
+        TimeToOrderConfirmation,
+        GetCoordinatesChange,
+        OrdersPlacedWithCoordinatesChange,
+        GetOrderDetailsPerFullVisitorId,
+    )
 
     from constants import config
     from application import application
@@ -18,5 +25,14 @@ if __name__ == "__main__":
     api.add_resource(TotalSessions, "/api/v1/total-sessions")
     api.add_resource(SessionsPerUser, "/api/v1/sessions-per-user")
     api.add_resource(TimeToOrderConfirmation, "/api/v1/time-to-order-confirmation")
+    api.add_resource(GetCoordinatesChange, "/api/v1/get-coordinates-change")
+    api.add_resource(
+        OrdersPlacedWithCoordinatesChange,
+        "/api/v1/orders-placed-with-coordinates-change",
+    )
+    api.add_resource(
+        GetOrderDetailsPerFullVisitorId,
+        "/api/v1/get-order-details-per-full-visitor-id",
+    )
 
     application.run(host="0.0.0.0", debug=True)
