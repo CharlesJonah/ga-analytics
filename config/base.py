aaca73091420e8a34840229345937facebc00d15
CharlesJonah <mutua.charles48@gmail.com>
2021-09-08 17:30:39 +0300

    ensure gracefull failure incase some environment variables are missing

--- config/base.py
+++ config/base.py
@@ -4,6 +4,6 @@ import os
 class BaseConfig:
     """This class configures the parameters to be used across different enviroments"""
 
-    SECRET_KEY = os.environ["SECRET_KEY"]
-    PROJECT_ID = os.environ["PROJECT_ID"]
-    GOOGLE_APPLICATION_CREDENTIALS = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    PROJECT_ID = os.environ.get("PROJECT_ID")
+    GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
